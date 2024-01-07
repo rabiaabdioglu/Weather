075b8d8cba6824912162f1982c47bab00627517c
Rabia Abdioğlu <rabiabdglu@gmail.com>
2024-01-07 12:34:12 +0300

    Initial commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Rabia Abdioğlu on 7.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

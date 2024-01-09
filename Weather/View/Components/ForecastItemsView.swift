fa60123e8625f63a7fca8bc3fb49d23a2eedabee
Rabia Abdioğlu <rabiabdglu@gmail.com>
2024-01-09 23:56:19 +0300

    Search Bar

---
+++ Weather/View/Components/ForecastItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastItemsView.swift
+//  Weather
+//
+//  Created by Rabia Abdioğlu on 9.01.2024.
+//
+
+import SwiftUI
+
+struct ForecastItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForecastItemsView()
+}
